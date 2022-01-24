7872d4865bf31e30991dec42b8c7dbf2a7e42817
gomg0m <kjy@kitech.re.kr>
2022-01-24 13:03:17 +0900

    CreateList 포함

---
+++ src/components/CreatList.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react"
+import styled from "styled-components"
+import { Button } from "antd"
+import DetailList from "./DetailLIst"
+
+const CreateListDiv = styled.div`
+  padding: 3rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
+
+const CreateList = () => {
+  const [countList, setCountList] = useState([0])
+
+  const onAddDetailDiv = () => {
+    let countArr = [...countList]
+    let counter = countArr.slice(-1)[0]
+    counter += 1
+    countArr.push(counter)	// index 사용 X
+    // countArr[counter] = counter	// index 사용 시 윗줄 대신 사용	
+    setCountList(countArr)
+  }
+
+  return (
+    <CreateListDiv>
+      <DetailList countList={countList} />
+      <Button onClick={onAddDetailDiv}>
+          추가
+      </Button>
+    </CreateListDiv>
+  )
+}
+export default CreateList
