457f4a34e89b5767235124e3050b3da89e1b2edd
gomg0m <kjy@kitech.re.kr>
2022-01-24 20:05:55 +0900

    API 없이

--- pages/card.js
+++ pages/card.js
@@ -2,10 +2,12 @@ import * as React from 'react';
 import {Card, CardContent, CardMedia, Typography, CardActionArea} from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import {Grid} from 'semantic-ui-react';
+import { isUnitless } from '@mui/material/styles/cssUtils';
 
 export default function ActionAreaCard() {
   
-  const [list, setListData] = useState([]);
+  const [list, setListData] = useState(["image/IU.jpg","image/BTS.jpg","image/IU.jpg","image/BTS.jpg"]);
 
   const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
   
@@ -17,34 +19,40 @@ export default function ActionAreaCard() {
     });
   }
 
-  useEffect(()=>{
-    getData();
-  },[]);
+  //useEffect(()=>{
+  //  getData();
+  //},[]);
 
-  list.slice(0,1);
 
   return (
     <div>
-    { list.map((item)=>(
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image={item.image_link}
-            alt="IU"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              IU
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {item.name}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      )) }
-      </div>
+      <Grid columns={3}>
+        <Grid.Row>
+          { 
+          list.map((item)=>(
+            <Grid.Column>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={item}
+                    alt="IU"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      IU
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {item.name}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid.Column>
+          )) }
+        </Grid.Row>
+      </Grid>
+    </div>
   );
 } 
